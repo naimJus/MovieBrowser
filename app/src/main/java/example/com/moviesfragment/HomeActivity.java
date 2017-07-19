7ebe8e73668cec68bc48e602331c6c38922e8a4a
naim <jusufi.naim@outlook.com>
2017-07-19 14:49:34 +0200

    Filter configuration

--- app/src/main/java/example/com/moviesfragment/HomeActivity.java
+++ app/src/main/java/example/com/moviesfragment/HomeActivity.java
@@ -1,9 +1,11 @@
 package example.com.moviesfragment;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,24 +23,33 @@ public class HomeActivity extends ListActivity {
     MoviesDataSource moviesDataSource;
     ListView listView;
     List<Movie> getMovies;
+    String filter;
+    public static final String SORTED = "filter";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v(LOG, "onCreate() ");
         listView = getListView();
 
         moviesDataSource = new MoviesDataSource(this);
         moviesDataSource.open();
 
-        Bundle b = getIntent().getExtras();
-        if (b != null) {
-            String s = b.getString(MainActivity.FILTER);
-            getMovies = moviesDataSource.sortBy(s);
+        View footerView = ((LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.footer_layout, null, false);
+        listView.addFooterView(footerView);
+
+        if (savedInstanceState != null) {
+            filter = savedInstanceState.getString(SORTED);
+            getMovies = moviesDataSource.sortBy(filter);
         } else {
-            getMovies = moviesDataSource.getAllMovies();
-//        Log.v(LOG, movies.get(1).toString());
+            Bundle b = getIntent().getExtras();
+            if (b != null) {
+                filter = b.getString(MainActivity.FILTER);
+                getMovies = moviesDataSource.sortBy(filter);
+            } else {
+                getMovies = moviesDataSource.getAllMovies();
+            }
         }
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -57,6 +68,13 @@ public class HomeActivity extends ListActivity {
             }
         });
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(SORTED, filter);
+    }
+
     @Override
     protected void onRestart() {
         super.onRestart();
@@ -73,17 +91,33 @@ public class HomeActivity extends ListActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        String filternew;
         switch (item.getItemId()) {
             case R.id.sortByName:
-                getMovies = moviesDataSource.sortBy(MovieSQLiteHelper.KEY_NAME + " ASC");
+                if (filter.equals(MovieSQLiteHelper.KEY_NAME + " ASC")){
+                    filter = MovieSQLiteHelper.KEY_NAME + " DESC";
+                } else {
+                    filter = MovieSQLiteHelper.KEY_NAME + " ASC";
+                }
+                getMovies = moviesDataSource.sortBy(filter);
                 refreshAdapter();
                 return true;
             case R.id.sortByRating:
-                getMovies = moviesDataSource.sortBy(MovieSQLiteHelper.KEY_RATING + " ASC");
+                if (filter.equals(MovieSQLiteHelper.KEY_RATING + " ASC")){
+                    filter = MovieSQLiteHelper.KEY_RATING + " DESC";
+                } else {
+                    filter = MovieSQLiteHelper.KEY_RATING + " ASC";
+                }
+                getMovies = moviesDataSource.sortBy(filter);
                 refreshAdapter();
                 return true;
             case R.id.sortByYear:
-                getMovies = moviesDataSource.sortBy(MovieSQLiteHelper.KEY_YEAR + " ASC");
+                if (filter.equals(MovieSQLiteHelper.KEY_YEAR + " ASC")){
+                    filter = MovieSQLiteHelper.KEY_YEAR + " DESC";
+                } else {
+                    filter = MovieSQLiteHelper.KEY_YEAR + " ASC";
+                }
+                getMovies = moviesDataSource.sortBy(filter);
                 refreshAdapter();
                 return true;
             default:
