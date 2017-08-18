10470ee7867456ca9e9e84c4428b7e013f94d8eb
naim <jusufi.naim@outlook.com>
2017-08-18 11:33:26 +0200

    Add torrent links to database

--- app/src/main/java/example/com/moviesfragment/MoviesDataSource.java
+++ app/src/main/java/example/com/moviesfragment/MoviesDataSource.java
@@ -7,11 +7,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MoviesDataSource {
     private SQLiteDatabase database;
     private MovieSQLiteHelper dbHelper;
+    private static final String LOG = MoviesDataSource.class.getSimpleName();
 
     MoviesDataSource(Context context) {
         dbHelper = MovieSQLiteHelper.getsInstance(context);
@@ -45,12 +47,17 @@ public class MoviesDataSource {
     }
 
     List<Movie> searchMovies(String[] search) {
+        if (search[3].equalsIgnoreCase("all"))
+            search[3] = "0";
+//        if (search[2].equalsIgnoreCase("all"))
+//            search[2] =
+
         Cursor cursor = database.query(MovieSQLiteHelper.TABLE_NAME,
                 null,
-                MovieSQLiteHelper.KEY_NAME + " LIKE " + "'%" + search[0] + "%' AND " +
-//                MovieSQLiteHelper.KEY_QUALITY + "=" + search[1] + "AND " +
-//                MovieSQLiteHelper.KEY_GENRE + "=" + search[2] + "AND " +
-                MovieSQLiteHelper.KEY_RATING + ">=" + search[3],
+                MovieSQLiteHelper.KEY_NAME + " LIKE " + search[0] + " AND " +
+//                MovieSQLiteHelper.KEY_QUALITY + "=" + search[1] + " AND " +
+                        MovieSQLiteHelper.KEY_GENRE + "=" + search[2] + " AND " +
+                        MovieSQLiteHelper.KEY_RATING + ">=" + search[3],
                 null,
                 null,
                 null,
@@ -71,13 +78,17 @@ public class MoviesDataSource {
                 movie.setImageUrl(cursor.getString(4));
                 movie.setRating(cursor.getFloat(5));
                 movie.setTrailerCode(cursor.getString(4));
+                movie.setGenre(cursor.getString(cursor.getColumnIndex(MovieSQLiteHelper.KEY_GENRE)));
+                movie.setUrl720p(cursor.getString(cursor.getColumnIndex(MovieSQLiteHelper.KEY_720P)));
+                movie.setUrl1080p(cursor.getString(cursor.getColumnIndex(MovieSQLiteHelper.KEY_1080P)));
+                movie.setUrl3d(cursor.getString(cursor.getColumnIndex(MovieSQLiteHelper.KEY_3D)));
                 movies.add(movie);
             }
         }
         return movies;
     }
 
-    long createMovie(long id, String name, String description, int year, String imageUrl, float rating, String trailerCode) {
+    long createMovie(long id, String name, String description, int year, String imageUrl, float rating, String trailerCode, String genre, HashMap<String, String> torrents) {
         long resultId = -1;
         ContentValues values = new ContentValues();
         values.put(MovieSQLiteHelper.KEY_ID, id);
@@ -87,6 +98,10 @@ public class MoviesDataSource {
         values.put(MovieSQLiteHelper.KEY_IMAGE_URL, imageUrl);
         values.put(MovieSQLiteHelper.KEY_RATING, rating);
         values.put(MovieSQLiteHelper.KEY_TRAILER, trailerCode);
+        values.put(MovieSQLiteHelper.KEY_GENRE, genre);
+        values.put(MovieSQLiteHelper.KEY_720P, torrents.get("720p"));
+        values.put(MovieSQLiteHelper.KEY_1080P, torrents.get("1080p"));
+        values.put(MovieSQLiteHelper.KEY_3D, torrents.get("3D"));
         resultId = database.insertWithOnConflict(MovieSQLiteHelper.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
         return resultId;
     }
