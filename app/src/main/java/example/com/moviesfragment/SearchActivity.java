dcff05b72629f7ef81c19c99a5868e7ab05e37e9
naim <jusufi.naim@outlook.com>
2017-08-19 14:00:32 +0200

    Set up search parameters

--- app/src/main/java/example/com/moviesfragment/SearchActivity.java
+++ app/src/main/java/example/com/moviesfragment/SearchActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.util.HashMap;
+
 public class SearchActivity extends Activity {
 
     Spinner qualitySpinner, genreSpinner, ratingSpinner, orderBySpinner;
@@ -15,12 +17,12 @@ public class SearchActivity extends Activity {
     Button searchBtn;
     public static final String SEARCH = "search";
     private static final String LOG = SearchActivity.class.getSimpleName();
+    HashMap<String, String> searchParams = new HashMap<>(4);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
         qualitySpinner = (Spinner) findViewById(R.id.qualitySpinner);
         genreSpinner = (Spinner) findViewById(R.id.genreSpinner);
         ratingSpinner = (Spinner) findViewById(R.id.ratingSpinner);
@@ -28,18 +30,33 @@ public class SearchActivity extends Activity {
         searchBtn = (Button) findViewById(R.id.searchBtn);
         searchET = (EditText) findViewById(R.id.searchET);
 
-
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String[] searchParameters = {
-                        searchET.getText().toString(),
-                        qualitySpinner.getSelectedItem().toString(),
-                        genreSpinner.getSelectedItem().toString(),
-                        ratingSpinner.getSelectedItem().toString()};
+//                StringBuilder sb = new StringBuilder("'% ");
+//                sb.append(searchET.getText().toString());
+//                sb.append(" %'");
+//                String[] searchParameters = {
+//                        sb.toString(),
+//                        qualitySpinner.getSelectedItem().toString(),
+//                        genreSpinner.getSelectedItem().toString(),
+//                        ratingSpinner.getSelectedItem().toString()};
+
+                String quality = qualitySpinner.getSelectedItem().toString();
+                String genre = genreSpinner.getSelectedItem().toString();
+                String rating = ratingSpinner.getSelectedItem().toString();
+                String order = orderBySpinner.getSelectedItem().toString();
+                String search = searchET.getText().toString();
+
+                searchParams.put("Quality", quality);
+                searchParams.put("Genre", genre);
+                searchParams.put("Rating", rating);
+                searchParams.put("Order", order);
+                searchParams.put("Search", search);
+
 
                 Intent intent = new Intent(SearchActivity.this, HomeActivity.class);
-                intent.putExtra(SEARCH, searchParameters);
+                intent.putExtra(SEARCH, searchParams);
                 startActivity(intent);
             }
         });
