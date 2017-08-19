32f87ba80809b2ad3095bdef3ddc5d20ae3c1b05
naim <jusufi.naim@outlook.com>
2017-08-19 14:01:19 +0200

    Add new Activity to show search results

---
+++ app/src/main/java/example/com/moviesfragment/SearchResultsActivity.java
@@ -0,0 +1,13 @@
+package example.com.moviesfragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchResultsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+}
