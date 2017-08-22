d5ec5cd942ad02b1d83e24dc7ef9bbd4f016eef0
naim <jusufi.naim@outlook.com>
2017-08-22 10:47:06 +0200

    Download Torrents

--- app/src/main/java/example/com/moviesfragment/MovieActivity.java
+++ app/src/main/java/example/com/moviesfragment/MovieActivity.java
@@ -1,6 +1,10 @@
 package example.com.moviesfragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -15,6 +19,8 @@ public class MovieActivity extends YouTubeBaseActivity {
     private YouTubePlayerView youTubePlayerView;
     private YouTubePlayer.OnInitializedListener initializedListener;
     private static final String YOUTUBE_KEY = "AIzaSyBp9dpHGyl_0MUM8z_SwKPXeWEabVlUSKk";
+    private Button quality720Btn, quality1080Btn, quality3dBtn;
+    Button torrent;
 
     @Override
 
@@ -26,6 +32,9 @@ public class MovieActivity extends YouTubeBaseActivity {
         yearTV = (TextView) findViewById(R.id.movie_year);
         ratingTV = (TextView) findViewById(R.id.movie_rating);
         descriptionTV = (TextView) findViewById(R.id.movie_description);
+        quality720Btn = (Button) findViewById(R.id.quality720p);
+        quality1080Btn = (Button) findViewById(R.id.quality1080p);
+        quality3dBtn = (Button) findViewById(R.id.quality3d);
 
         // get the bundle from the intent
         //unwrap the bundle and get the movie;
@@ -36,6 +45,27 @@ public class MovieActivity extends YouTubeBaseActivity {
         yearTV.setText(String.valueOf(movie.getYear()));
         ratingTV.setText(String.valueOf(movie.getRating()));
         descriptionTV.setText(movie.getDescription());
+        final String url720 = movie.getUrl720p();
+        String url1080 = movie.getUrl1080p();
+        String url3d = movie.getUrl3d();
+
+        if (url720 == null)
+            quality720Btn.setVisibility(View.GONE);
+        if (url1080 == null)
+            quality1080Btn.setVisibility(View.GONE);
+        if (url3d == null)
+            quality3dBtn.setVisibility(View.GONE);
+
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+
+        quality720Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setData(Uri.parse(url720));
+                startActivity(intent);
+            }
+        });
         youTubePlayerView = (YouTubePlayerView) findViewById(R.id.playerYouTube);
         initializedListener = new YouTubePlayer.OnInitializedListener() {
             @Override
