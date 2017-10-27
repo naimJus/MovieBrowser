6d7cb27b6b7d265bc6aa3299fbfd0a66664382b2
Naim Jusufi <jusufi.naim@outlook.com>
2017-10-27 10:52:49 +0200

    coment the code in MainActivity.java and layout activity_main.xml, and add frame layout to contain fragment

--- app/src/main/java/example/com/moviesfragment/MainActivity.java
+++ app/src/main/java/example/com/moviesfragment/MainActivity.java
@@ -1,17 +1,16 @@
 package example.com.moviesfragment;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import com.facebook.stetho.Stetho;
 
 public class MainActivity extends Activity {
-    Button browseBtn, recentBtn, topBtn;
     public static final String FILTER = "TOPRATED";
+    Button browseBtn, recentBtn, topBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +18,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Stetho.initializeWithDefaults(this);
 
+//      Display the recent movies
+        FragmentManager manager = getFragmentManager();
+
+/*
         browseBtn = (Button) findViewById(R.id.browseBtn);
         recentBtn = (Button) findViewById(R.id.recentBtn);
         topBtn = (Button) findViewById(R.id.topBtn);
-
 
         browseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +36,7 @@ public class MainActivity extends Activity {
         recentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent intent = new Intent(MainActivity.this, MoviesListFragment.class);
                 intent.putExtra(FILTER, MovieSQLiteHelper.KEY_ID + " DESC");
                 startActivity(intent);
             }
@@ -42,9 +44,9 @@ public class MainActivity extends Activity {
         topBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                new GetMovieCountTwo(getApplicationContext(), MainActivity.this).execute("&sort_by=rating");
             }
         });
+*/
 
     }
 }
