6e95ccbe519d80a19d5c30643c0a218258dc72b3
Naim Jusufi <jusufi.naim@outlook.com>
2017-11-28 19:14:20 +0100

    Magnet button onClick now works. Running constructMagnetLink method which encodes Uri

--- app/src/main/java/example/com/moviesfragment/MovieActivity.java
+++ app/src/main/java/example/com/moviesfragment/MovieActivity.java
@@ -3,7 +3,6 @@ package example.com.moviesfragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +15,8 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 import com.squareup.picasso.Picasso;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 
@@ -58,22 +59,20 @@ public class MovieActivity extends YouTubeBaseActivity {
 
         downloadBtn = (Button) findViewById(R.id.downloadBtn);
         magnetBtn = (Button) findViewById(R.id.magnetBtn);
-
         movieImage = (ImageView) findViewById(R.id.movie_image);
 
 
-        // get the bundle from the intent
-        //unwrap the bundle and get the movie;
-
-
+//      The getMovie(String id) method returns List of Movies
+//      including separate object for each movie.
         int b = getIntent().getIntExtra(MoviesListFragment.BUNDLE, 1);
         List<Movie> movies = moviesDataSource.getMovie(b + "");
         movie = movies.get(0);
 
-        final HashMap<String, String> moviesMap = new HashMap<>();
+        final HashMap<String, Torrent> moviesMap = new HashMap<>();
+
         for (Movie m : movies) {
             Torrent t = m.getTorrent();
-            moviesMap.put(t.getQuality(), t.getUrl());
+            moviesMap.put(t.getQuality(), t);
         }
 
         if (moviesMap.containsKey("720p")) {
@@ -91,9 +90,6 @@ public class MovieActivity extends YouTubeBaseActivity {
                 radioButton3d.setChecked(true);
         }
 
-
-        Log.v(LOG, movie.toString());
-
         Picasso.with(this)
                 .load(movie.getMediumCoverImage())
                 .fit()
@@ -110,18 +106,9 @@ public class MovieActivity extends YouTubeBaseActivity {
         downloadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "";
-
-                if (radioButton720p.isChecked())
-                    url = moviesMap.get("720p");
-                if (radioButton1080p.isChecked())
-                    url = moviesMap.get("1080p");
-                if (radioButton3d.isChecked())
-                    url = moviesMap.get("3D");
-
+                String url = getUrlIfChecked(moviesMap);
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
-//                intent.setDataAndType(Uri.parse(url), "application/x-bittorrent");
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 startActivity(intent);
             }
@@ -130,7 +117,16 @@ public class MovieActivity extends YouTubeBaseActivity {
         magnetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String url = null;
+                try {
+                    url = constructMagnetLink(movie);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
 
+                Intent torrentIntent = new Intent(Intent.ACTION_VIEW);
+                torrentIntent.setData(Uri.parse(url));
+                startActivity(torrentIntent);
             }
         });
 
@@ -150,4 +146,50 @@ public class MovieActivity extends YouTubeBaseActivity {
         };
         youTubePlayerView.initialize(YOUTUBE_KEY, initializedListener);
     }
+
+    public String getUrlIfChecked(HashMap<String, Torrent> map) {
+        String url = null;
+        if (radioButton720p.isChecked())
+            url = map.get("720p").getUrl();
+        if (radioButton1080p.isChecked())
+            url = map.get("1080p").getUrl();
+        if (radioButton3d.isChecked())
+            url = map.get("3D").getUrl();
+
+        return url;
+    }
+
+    public String constructMagnetLink(Movie m) throws UnsupportedEncodingException {
+
+        String baseUrl = "magnet:?xt=urn:btih:" + m.getTorrent().getHash();
+        String titleLong =
+                m.getTitleLong()
+                        + " [" + m.getTorrent().getQuality()
+                        + "]" + " [YTS.AG] ";
+
+
+        StringBuilder trackersQuery = new StringBuilder(baseUrl);
+        trackersQuery.append("&dn=");
+        String titleQuery = URLEncoder.encode(titleLong, "utf-8");
+        trackersQuery.append(titleQuery);
+
+        String[] trackers =
+                {
+                        "udp://glotorrents.pw:6969/announce",
+                        "udp://tracker.openbittorrent.com:80",
+                        "udp://tracker.coppersurfer.tk:6969",
+                        "udp://tracker.leechers-paradise.org:6969",
+                        "udp://p4p.arenabg.com:1337",
+                        "udp://tracker.opentrackr.org:1337/announce",
+                        "udp://torrent.gresille.org:80/announce"
+                };
+
+        for (String s : trackers) {
+            trackersQuery.append("&tr=");
+            trackersQuery.append(URLEncoder.encode(s, "utf-8"));
+        }
+
+        return trackersQuery.toString();
+    }
 }
+
